e5f04387ce43d0b4cb7b0a7a4dc0aebb05844e87
Vivek Vinushanth <cvivekvinushanth@gmail.com>
2020-09-09 11:21:07 +0530

    Adding annotation,download endpoints

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 import os, time
+import uuid
 
-from flask import Flask, render_template, request, send_file
-
-# import blei_executable_and_tethne,FilterTweetDataByMonth,plotting,FilterTweetDataByDate,
+from flask import Flask, render_template, request, send_file,request, jsonify
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-
-@app.route("/")
-def main():
-    return render_template("index.html")
-
-
-@app.route("/annotate",methods=["GET"])
+@app.route("/annotate",methods=["POST"])
 def annotate():
-    print(APP_ROOT)
-    target = os.path.join(APP_ROOT, "/annotate")
+    target = "/".join([APP_ROOT,"toAnnotate"])
 
     if not os.path.isdir(target):
         os.mkdir(target)
 
+    # Obtain filename
     file = request.files['video']
     filename = file.filename
 
@@ -29,16 +22,24 @@ def annotate():
     if not (".mp4" in file.filename):
         return render_template("file Error.html")
 
+    id = uuid.uuid1()
+    # Save file
     if file:
-        destination = "/".join([target, filename])
-        file.save(destination)
+        # Adding to datastore/database
+        video_file = target+'/'+str(id)+'.mp4'
+        file.save(video_file)
 
-    #     Annotate video for emotions
+        # Annotate video for emotions
+        # if 'emo' in request.args:
+            # annotate_emo(id)
+        # if 'emo' in request.args:
+            # annotate_behav(id)
+        # if 'emo' in request.args:
+            # annotate_threat(id)
 
-    return render_template("index1.html", fileExists=True)
 
-
-
+    response = {"themobe_id":id,"video":filename,"video_status":"processing"}
+    return jsonify(response)
 
 @app.route("/viewResults")
 def viewResults(filename):
@@ -50,10 +51,14 @@ def viewResults(filename):
 
 
 @app.route("/download")
-def downloadFile(filename):
+def downloadFile():
     # For windows you need to use drive name [ex: F:/Example.pdf]
-    path = "static/03.png"
-    return send_file(path, as_attachment=True)
+    if 'themobe_id' in request.args:
+        id = request.args.get('themobe_id')
+        path =  "/".join([APP_ROOT,"toAnnotate"])
+        print(path)
+        video = path+'/'+id+'.mp4'
+    return send_file(video, as_attachment=True)
 
 
 if __name__ == "__main__":
